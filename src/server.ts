d50323428a5a02e878ecb03728715826ab288b41
Miguel L. Garcia <miguellg88@gmail.com>
2020-08-10 08:48:17 -0300

    ajuste deploy

--- src/server.ts
+++ src/server.ts
@@ -4,23 +4,6 @@ import routes from './routes'
 import * as firebase from 'firebase-admin'
 import * as serviceAccount from '../diversos-285721-44a640409e15.json'
 
-const params = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientC509CertUrl: serviceAccount.client_x509_cert_url
-}
-   
-firebase.initializeApp({
-  credential: firebase.credential.cert(params),
-})
-
 var app  = express();
 app.use(cors());
 app.use(express.json())
