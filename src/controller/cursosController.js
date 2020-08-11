707572bdda6b473e5aaca2ea16414a4046663206
Miguel L. Garcia <miguellg88@gmail.com>
2020-08-11 15:00:59 -0300

    Ajuste servico gravacao

--- src/controller/cursosController.js
+++ src/controller/cursosController.js
@@ -45,24 +45,8 @@ class CursosController {
         };
         const insertedIds = await connection_1.default('alunos').insert(item);
         const id = insertedIds[0];
-        this.notificar(token);
+        // this.notificar(token)
         return response.json(Object.assign({ id }, item));
-    }
-    async notificar(token) {
-        var message = {
-            notification: {
-                title: 'Cursos Online',
-                body: 'Curso encerrado! Agradecemos sua participação.'
-            },
-            token: token
-        };
-        firebase.messaging().send(message)
-            .then((response) => {
-            console.log('Mensagem enviada com sucesso:', response);
-        })
-            .catch((error) => {
-            console.log('Erro ao enviar mensagem:', error);
-        });
     }
 }
 exports.default = CursosController;
